1e62b80192e6fa97c111a1ed379625633362c0de
Hanke Chen <i@kokecacao.me>
2022-05-11 15:06:46 -0400

    :bug: commit login prompt widget

--- lib/widgets/login_prompt_widget.dart
+++ lib/widgets/login_prompt_widget.dart
@@ -4,32 +4,30 @@ import 'package:provider/provider.dart';
 import '../utils/theme_data_wrapper.dart';
 
 class LoginPrompt extends StatelessWidget {
-  LoginPrompt();
-
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            IconButton(
-              color: Provider.of<ThemeDataWrapper>(context, listen: false)
-                  .textColor,
-              focusColor:
-              Provider.of<ThemeDataWrapper>(context, listen: false)
-                  .highlightTextColor,
-              hoverColor:
-              Provider.of<ThemeDataWrapper>(context, listen: false)
-                  .fadeTextColor,
-              icon: Icon(Icons.login),
-              onPressed: () {
-                Navigator.of(context).pushNamed("/login_screen");
-              },
-            ),
-            Text("Please Log in first"),
-          ],
-        ),
+    // TODO
+    // Cannot use expanded, see https://stackoverflow.com/questions/54905388/incorrect-use-of-parent-data-widget-expanded-widgets-must-be-placed-inside-flex
+    // You should probably use Positioned.fill(key
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          IconButton(
+            color:
+                Provider.of<ThemeDataWrapper>(context, listen: false).textColor,
+            focusColor: Provider.of<ThemeDataWrapper>(context, listen: false)
+                .highlightTextColor,
+            hoverColor: Provider.of<ThemeDataWrapper>(context, listen: false)
+                .fadeTextColor,
+            icon: Icon(Icons.login),
+            onPressed: () {
+              Navigator.of(context).pushNamed("/login_screen");
+            },
+          ),
+          Text("Please Log in first"),
+        ],
       ),
     );
   }
